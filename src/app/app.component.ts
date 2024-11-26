c084a90a3cd4121889482c3dc891726184ec0d35
Oleg Dragun <okd.angular@gmail.com>
2024-11-27 00:32:24 +0300

    add header, banner, catalog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +11,14 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'mentoring-first-project';
+
+  readonly headerItem1 = 'Главная';
+  readonly headerItem2 = 'О компании';
+  readonly headerItem3 = 'Каталог';
+
+  readonly header2Item1 = 'Каталог';
+  readonly header2Item2 = 'Стройматериалы';
+  readonly header2Item3 = 'Инструменты';
+  readonly header2Item4 = 'Электрика';
+  readonly header2Item5 = 'Интерьер и одежда';
 }
