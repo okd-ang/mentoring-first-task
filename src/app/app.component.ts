a9db51778ca7f06f14de8772d925bda3f8b6af2b
Oleg Dragun <okd.angular@gmail.com>
2025-01-21 22:47:08 +0300

    all tasks up to and including task 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,36 @@
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { isNgTemplate } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+const aboutCompanyFn = (text: string) => text;
+const aboutCompany = aboutCompanyFn('Окомпании');
+
+const newPages: number[] = [5, 4, 3, 2, 1];
+
+const menuItems = [
+  'Каталог',
+  'Стройматериалы',
+  'Инструменты',
+  'Электрика',
+  'Интерьер и одежда',
+];
+const upperCaseMenuItems: string[] = menuItems.map((item: string) => {
+  return item.toUpperCase();
+});
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'mentoring-first-project';
+
+  isShowCatalog = true;
+  isShowBanner = true;
 
   readonly headerItem1 = 'Главная';
   readonly headerItem2 = 'О компании';
@@ -21,4 +41,19 @@ export class AppComponent {
   readonly header2Item3 = 'Инструменты';
   readonly header2Item4 = 'Электрика';
   readonly header2Item5 = 'Интерьер и одежда';
+
+  readonly newPages: number[] = newPages;
+
+  readonly aboutCompany = aboutCompany;
+
+  menuItems: string[] = upperCaseMenuItems;
+
+  isUpperCase: boolean = true;
+
+  changeMenuText(): void {
+    this.menuItems = upperCaseMenuItems.map((item) =>
+      this.isUpperCase ? item.toLowerCase() : item.toUpperCase()
+    );
+    this.isUpperCase = !this.isUpperCase;
+  }
 }
